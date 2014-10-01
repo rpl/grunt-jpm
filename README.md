a409e735d83fd3d46f278864da3d1b26951356c0
Luca Greco <luca.greco@alcacoop.it>
2014-10-01 12:31:18 +0000

    minimal grunt jpm plugin: xpi and run tasks

---
+++ README.md
@@ -0,0 +1,60 @@
+# grunt-jpm v.0.1.0
+
+> Run and build Firefox Addon using JPM from your Gruntfile.js
+
+## Getting Started
+This plugin requires Grunt `~0.4.0` 
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-jpm --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-jpm');
+```
+
+### Settings
+
+This plugin is configured using the "jpm.options" section of your *Gruntfile.js* config:
+
+```js
+grunt.initConfig({
+  ...
+  jpm: {
+    options: {
+      src: "./src/",
+      xpi: "./tmp/"
+    }
+});
+```
+
+#### src
+Type: `String`
+
+This defines the relative path to the add-on sources.
+
+#### xpi
+Type: `String`
+
+This defines the relative path to the directory where the built xpi will be written.
+
+## *jpm:xpi* task
+_Run this task with the `grunt jpm:xpi` command to build an xpi from your addon sources._
+
+- use the **--verbose** and *-d* command line option to run the tasks verbosely
+
+## *jpm:run* task
+_Run this task with the `grunt jpm:run` command to build an xpi from your addon sources._
+
+Optionally you can:
+
+- define the *FIREFOX_BIN* environment var or use the *--firefox-bin* command line option
+  to specify the absolute path to the Firefox binary
+- define the *FIREFOX_PROFILE* environment var or use the *--firefox-profile* command line option
+  to specify the absolute path to a Firefox profile
+- use the **--firefox-debugger** command line option to automatically start the Firefox Developer Toolbox connected to the add-on
+- use the **--verbose** and *-d* command line option to run the tasks verbosely
---
+++ tasks/jpm.js
@@ -0,0 +1,91 @@
+var path = require('path');
+var jpm_utils = require("jpm/lib/utils");
+var jpm_xpi = require("jpm/lib/xpi");
+var jpm_run = require("jpm/lib/run");
+
+const XPI_PATH = "./tmp/";
+
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.registerTask('jpm:xpi', "build firefox xpi", function() {
+    var jpm_env = {};
+
+    try {
+      jpm_env = prepareJPMGruntTask();
+    } catch(e) {
+      grunt.log.error("Error preparing JPM Grunt Task: " + e);
+      return;
+    }
+
+    var done = this.async();
+
+    var manifest = jpm_env.manifest;
+    var dirs = jpm_env.dirs;
+
+    jpm_xpi(manifest, {
+      verbose: grunt.option('debug'),
+      xpiPath: dirs.xpi
+    }).then(function(res) {
+      grunt.log.ok("Generated XPI: ", res);
+    }, function(e) {
+      grunt.log.error("Error during XPI build:", e);
+    }).then(function () {
+      process.chdir(dirs.old);
+      done();
+    });
+  });
+
+  grunt.registerTask('jpm:run', "run firefox addon", function() {
+    var jpm_env = {};
+
+    try {
+      jpm_env = prepareJPMGruntTask();
+    } catch(e) {
+      grunt.log.error("Error preparing JPM Grunt Task: " + e);
+      return;
+    }
+
+    var done = this.async();
+
+    var manifest = jpm_env.manifest;
+    var dirs = jpm_env.dirs;
+
+    jpm_run(jpm_env.manifest, {
+      verbose: grunt.option('debug'),
+      debug: grunt.option('firefox-debugger'),
+      profile: grunt.option('firefox-profile') || process.env.FIREFOX_PROFILE,
+      binary: grunt.option('firefox-bin') || process.env.FIREFOX_BIN
+    }).then(null, function(e) {
+      grunt.log.error("Error running Firefox:", e);
+    });
+  });
+
+  function prepareJPMGruntTask() {
+    var grunt_config = grunt.config(["jpm", "options"]);
+
+    grunt.verbose.ok("Loading config", grunt_config);
+
+    var dirs = resolveDirsFromConfig(grunt_config);
+
+    grunt.verbose.ok('Moving from directory: ' + process.cwd());
+    grunt.verbose.ok('Moving to directory: ' + dirs.src);
+
+    process.chdir(path.join(dirs.old, dirs.src));
+    grunt.verbose.ok('New directory: ' + process.cwd());
+
+    var manifest = jpm_utils.getManifest();
+
+    return { manifest: manifest, dirs: dirs };
+  }
+};
+
+function resolveDirsFromConfig(grunt_config, name) {
+  var old_dir = process.cwd();
+
+  return {
+    old: old_dir,
+    src: grunt_config["src"],
+    xpi: path.join(old_dir, grunt_config["xpi"] || XPI_PATH)
+  };
+}
